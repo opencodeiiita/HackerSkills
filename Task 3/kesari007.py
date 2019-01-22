f0b8a66052c12afc1e8a13650708cfccc8d9c8b9
kesari007 <ankurkesharwani3095@gmail.com>
2019-01-22 21:30:07 +0530

    Data now in required format

--- Task 3/kesari007.py
+++ Task 3/kesari007.py
@@ -39,5 +39,5 @@ for i in range(25):
         "Image_Url" : url_list[i]
     }
     item_list.append(data)
-    with open('data.json', 'a') as outfile:
-        json.dump(data, outfile, indent = 4)
+with open('data.json', 'a') as outfile:
+    json.dump(item_list, outfile, indent = 4)
