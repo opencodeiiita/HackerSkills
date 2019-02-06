378ef940c7df80bfe5ff6e5d764121875bcf4f6f
thesmallstar <42006277+thesmallstar@users.noreply.github.com>
2019-02-06 19:12:43 +0530

    Update turtle.py

--- task 10/thesmallstar/turtle.py
+++ task 10/thesmallstar/turtle.py
@@ -1,13 +1,8 @@
 import turtle
-import random
 a = turtle.Turtle()
-while True:
-  a.speed(-1000)
-  sz = 2 
-  ang = random.randint(1, 302)
-  for i in range(500):
-    a.right(ang)
+a.speed(-1000)
+sz = 2 
+for i in range(500):
+    a.right(100)
     a.forward(sz)
     sz = sz+0.5
-  a.clear()
-  a.reset()
