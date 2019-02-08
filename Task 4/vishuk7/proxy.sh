1169cc75c62a8421de461bac925c7b8b47d7169b
vishnuk7 <vishnu.kraman88@gmail.com>
2019-02-09 01:29:30 +0530

    Shell Script to change proxy #7

---
+++ Task 4/vishuk7/proxy.sh
@@ -0,0 +1,6 @@
+export http_proxy="http://127.0.0.7:8080";
+export https_proxy="http://127.0.0.7:8080";
+export ftp_proxy="http://127.0.0.7:8080";
+export HTTP_PROXY="http://127.0.0.7:8080";
+export HTTPS_PROXY="http://127.0.0.7:8080";
+export FTP_PROXY="http://127.0.0.7:8080"
