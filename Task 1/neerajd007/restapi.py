b498bc8d9a52acaba2468da53e4f0d25c361c971
Neeraj Dwivedi <neerajd1605@gmail.com>
2019-02-08 17:46:54 +0530

    Task-1 Issue #1 Access Rest API (#75)
    * Task-1 Issue #1 Access Rest API
    
    * Added Changes
    
    * Fixed Url

---
+++ Task 1/neerajd007/restapi.py
@@ -0,0 +1,21 @@
+import requests, re, json
+
+print("Enter the description of the job:")
+desc=raw_input()
+print("Enter the location of the job:")
+location=raw_input()
+resp = requests.get("https://jobs.github.com/positions.json?description="+desc+"&location="+location)
+
+
+if resp:
+	for data in resp:
+		print("Title :" + data+"\n")
+		cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', data)
+        print("Description :" + cleantext +"\n")
+        print("Url :" + data+"\n")
+        print("-----------------------")
+		
+
+else:
+	print("No jobs found")
