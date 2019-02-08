18ca433b0b75537645d5cebb1f9f3ffdef7162f0
Lalit Sharma <38866385+Rattlecode17sh@users.noreply.github.com>
2019-02-08 22:07:49 +0530

    Update app.py

--- Task 11/Task 11(Lalit)/app.py
+++ Task 11/Task 11(Lalit)/app.py
@@ -12,18 +12,14 @@ with open('quotes.json') as data_file:
     data_file.close()
 quotes = data['quotes']    
 
-    
 @app.route('/')
 def index():
     return 'Hello! How are you??'
-
 
 @app.route('/quotes/api/random', methods=['GET'])
 def get_quote_random():
     q_id=randint(0,len(quotes)-1)
     return jsonify({'quotes': quotes[q_id]})
-    
-    
     
 @app.route('/quotes/api/quote/<int:quote_id>', methods=['GET'])
 def get_quote_id(quote_id):
@@ -32,8 +28,6 @@ def get_quote_id(quote_id):
         abort(404)
     return jsonify({'quotes': quote})
 
-    
-    
 @app.route('/quotes/api/quote/<author>', methods=['GET'])
 def get_quote_author(author):
     quote = [quote for quote in quotes if quote['author'] == author]
@@ -41,13 +35,9 @@ def get_quote_author(author):
         abort(404)
     return jsonify({'quotes': quote})
 
-    
-
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'quotes':{'id': u'error','quote': u'error','author': u'error'}}), 404)
 
-    
-      
 if __name__ == '__main__':
     app.run(debug=True)
