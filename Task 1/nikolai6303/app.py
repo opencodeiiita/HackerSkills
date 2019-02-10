a68f9932a5fcd2486b40b561ae67daa39535bf26
nikolai6303 <40137469+nikolai6303@users.noreply.github.com>
2019-02-10 05:30:00 +0530

    Add files via upload

---
+++ Task 1/nikolai6303/app.py
@@ -0,0 +1,21 @@
+import requests
+
+location=input("enter location")
+description=input("description")
+
+URL = "https://jobs.github.com/positions.json?description="+description+"&location=san+"+location
+
+r = requests.get(url = URL)
+
+data = r.json() 
+
+for i in range(3):
+    title=data[i]['title']
+    description=data[i]['description']
+    link=data[i]['url']
+    print(title)
+    print(description)
+    print(link)
+    print("\n")
+
+
